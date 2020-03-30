60361b6432258c733ec3b17aeb66a7f719c725fa
radhigulati <radhigulati@gmail.com>
2020-03-30 13:36:11 -0700

    subtract tests

--- Test/index.test.js
+++ Test/index.test.js
@@ -1,5 +1,11 @@
 const sum = require("./index");
+const sub = require("./index");
+
 
 test("adds 1 + 2 to equal 3", () => {
   expect(sum(1, 2)).toBe(3);
+});
+
+test("subtracts 5 - 4 to equal 1", () => {
+  expect(sub(5, 4)).toBe(1);
 });
