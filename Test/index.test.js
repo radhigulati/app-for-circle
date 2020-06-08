f162c6fa24d510b258998e6205bb277c6c393d59
Radhika Gulati <radhigulati@gmail.com>
2020-06-08 13:18:38 -0700

    changed test

--- Test/index.test.js
+++ Test/index.test.js
@@ -3,7 +3,7 @@ const sum = require("./index");
 
 
 test("adds 1 + 2 to equal 3", () => {
-  expect(sum(1, 2)).toBe(3);
+  expect(sum(7, 3)).toBe(10);
 });
 
 test("subtracts 5 + 4 to equal 1", () => {
